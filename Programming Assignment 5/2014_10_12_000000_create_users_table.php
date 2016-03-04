e742b9b9c51275fda0a6e8bcf288de982256aafb
annievuvu <anvu94@gmail.com>
2016-03-04 16:49:30 -0500

    assignment 5 (though I have no idea what to do with the models)

---
+++ Programming Assignment 5/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('is_authorized')->default(false);
+            $table->boolean('active')->default(true);
+            $table->string('name');
+            $table->string('phone_num',10);
+            $table->string('user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
